858424c48a208d0289e2aad422f9a97bd49de0ef
adeltahenti <adel.tahenti@citycable.ch>
2018-08-28 18:57:00 +0200

    Some changes in appes6.js

--- js/appes6.js
+++ js/appes6.js
@@ -1,5 +1,5 @@
 class Book {
-  constructor(titlt, author, isbn) {
+  constructor(title, author, isbn) {
     this.title = title;
     this.author = author;
     this.isbn = isbn;
@@ -7,7 +7,7 @@ class Book {
 }
 
 class UI {
-  addBookList(book) {
+  addBookToList(book) {
     const list = document.getElementById('book-list');
     // Create tr element
     const row = document.createElement('tr');
@@ -54,3 +54,49 @@ class UI {
     document.getElementById('title').focus();
   }
 }
+
+// Event listeners
+// Event listener for add book
+document.getElementById('book-form').addEventListener('submit', function (e) {
+  // Get form values
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const isbn = document.getElementById('isbn').value;
+
+  // Instantiate book
+  const book = new Book(title, author, isbn);
+
+  // Instantiate UI 
+  const ui = new UI();
+
+  // Validate
+  if (title === '' || author === '' || isbn === '') {
+    // Error alert
+    ui.showAlert('Please fill in all fields', 'error');
+  } else {
+    // Add book to list
+    ui.addBookToList(book);
+
+    // Show success
+    ui.showAlert('Book Added...', 'success');
+
+    // Clear fields
+    ui.clearFields();
+  }
+
+
+  e.preventDefault();
+});
+
+// Event listener for delete
+document.getElementById('book-list').addEventListener('click', function (e) {
+  // Instantiate UI 
+  const ui = new UI();
+
+  ui.deleteBook(e.target);
+
+  // Show alert
+  ui.showAlert('Book removed...', 'success');
+
+  e.preventDefault();
+});
