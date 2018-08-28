4155e41a212fc31204240f91d7e6ee644f3cf717
adeltahenti <adel.tahenti@citycable.ch>
2018-08-28 10:21:12 +0200

    Event Add book to the list is running

---
+++ js/app.js
@@ -0,0 +1,56 @@
+// Book constructor
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+
+// UI constructor
+function UI() {}
+
+// UI prototype
+UI.prototype.addBookToList = function (book) {
+  const list = document.getElementById('book-list');
+  // Create tr element
+  const row = document.createElement('tr');
+  // Insert cols
+  row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href=".#" class="delete" title="Delete Book"><i class="fas fa-times"></i></a></td>
+  `;
+  list.appendChild(row);
+};
+
+// Clear field
+UI.prototype.clearFields = function () {
+  // Clear inputs
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+  document.getElementById('isbn').value = '';
+  // Get focus
+  document.getElementById('title').focus();
+}
+
+// Event listeners
+document.getElementById('book-form').addEventListener('submit', function (e) {
+  // Get form values
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const isbn = document.getElementById('isbn').value;
+
+  // Instantiate book
+  const book = new Book(title, author, isbn);
+
+  // Instantiate UI 
+  const ui = new UI();
+
+  // Add book to list
+  ui.addBookToList(book);
+
+  // Clear fields
+  ui.clearFields();
+
+  e.preventDefault();
+});
